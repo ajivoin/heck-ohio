38562865347a7b67123bc41ab6943c3c3742d08a
Andrew Jivoin <andrew@jivoin.com>
2018-10-27 17:39:22 -0400

    @ajivoin | Fixes up the code. Improves README.

--- Business IoT Thermostat/Form1.cs
+++ Business IoT Thermostat/Form1.cs
@@ -28,11 +28,17 @@ namespace Business_IoT_Thermostat
     {
         static int upCount = 0;
         static int runCount = 0;
-        static object lockObj = new object();
+
         const bool resolveNames = false;
+
         const int MAX_IN_RANGE = 10;
+        const int MIN_IN_RANGE = 2;
+
         const int TRIES_PER_IP = 3;
         const int PING_TIME = 500;
+        const int SAMPLE_COUNT = 1;
+
+        const string BASE_IP = "192.168.137.";
 
         public Form1()
         {
@@ -41,15 +47,16 @@ namespace Business_IoT_Thermostat
         }
         private void button1_Click(object sender, EventArgs e)
         {
-           // var localComputers = NetworkComputer.GetLocalNetwork();
+            //var localComputers = NetworkComputer.GetLocalNetwork();
             //Ping pingSender = new Ping();
             //for (int i = 0; i < 255; i++)
             //{
-            //    string adr = "192.168.137."+i.ToString();
+            //    string adr = "192.168.137." + i.ToString();
             //    IPAddress address = IPAddress.Parse(adr);
             //    Application.DoEvents();
             //    PingReply reply = pingSender.Send(address);
-            //    if (reply.Status == IPStatus.Success){
+            //    if (reply.Status == IPStatus.Success)
+            //    {
             //        textBox1.Text = adr + textBox1.Text;
             //        Application.DoEvents();
             //    }
@@ -63,14 +70,7 @@ namespace Business_IoT_Thermostat
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //var startTimeSpan = TimeSpan.Zero;
-            //var periodTimeSPan = TimeSpan.FromMilliseconds(500);
-            //var timer = new System.Threading.Timer((_) =>
-            //{
-            //    upCount = 0;
-            //    pingThing();
-            //}, null, startTimeSpan, periodTimeSPan);
-            for (int i = 0; i < 1; i++)
+            for (int i = 0; i < SAMPLE_COUNT; i++)
             {
                 upCount = 0;
                 pingThing();
@@ -82,16 +82,13 @@ namespace Business_IoT_Thermostat
             Stopwatch sw = new Stopwatch();
             sw.Start();
             runCount = 0;
-            string ipBase = "192.168.137.";
             Ping p = new Ping();
             PingReply pingReply;
-            //p.PingCompleted += new PingCompletedEventHandler(p_PingCompleted);
-            //ideal code
             List<string> ipList = new List<string>();
-            int MAX_IN_RANGE = 10;
-            int TRIES_PER_IP = 3;
-            int PING_TIME = 500;
-            for (int i = 2; i < MAX_IN_RANGE; i++) { ipList.Add(ipBase + i.ToString()); }
+            for (int i = MIN_IN_RANGE; i < MAX_IN_RANGE; i++)
+            {
+                ipList.Add(BASE_IP + i.ToString());
+            }
             foreach (var ip in ipList)
             {
                 Console.WriteLine("Running for {0}", ip);
@@ -109,7 +106,7 @@ namespace Business_IoT_Thermostat
                     }
                     if (replies.Count() > 0)
                     {
-                        Console.WriteLine("Pinging {0} was great success. Time: {1}", ip, replies.Sum(x => x.RoundtripTime));
+                        Console.WriteLine("Pinging {0} was great success. Time: {1} ms", ip, replies.Sum(x => x.RoundtripTime));
                         upCount++;
                     }
                 }
@@ -118,41 +115,12 @@ namespace Business_IoT_Thermostat
                     Console.WriteLine("Pinging {0} failed. (Null Reply object?)", ip);
                     continue;
                 }
-                //Ping p = new Ping();
-                //p.PingCompleted += new PingCompletedEventHandler(p_PingCompleted);
-                //p.SendAsync(ip, 500, ip);
-                //Application.DoEvents();
-
+                Application.DoEvents();
             }
 
 
             sw.Stop();
-            TimeSpan span = new TimeSpan(sw.ElapsedTicks);
             Console.WriteLine("Took {0} milliseconds. {1} hosts active.", sw.ElapsedMilliseconds, upCount);
         }
-        //static void p_PingCompleted(object sender, PingCompletedEventArgs e)
-        //{
-        //    string ip = (string)e.UserState;
-        //    lock (lockObj)
-        //    {
-        //        runCount++;
-        //    }
-        //    if (e.Reply != null && e.Reply.Status == IPStatus.Success)
-        //    {
-        //        if (e.Reply.RoundtripTime > 2)
-        //        {
-        //            Console.WriteLine("{0} is up: ({1} ms)", ip, e.Reply.RoundtripTime);
-        //            lock (lockObj)
-        //            {
-        //                upCount++;
-        //            }
-        //        }
-        //    }
-        //    else if (e.Reply == null)
-        //    {
-        //        Console.WriteLine("Pinging {0} failed. (Null Reply object?)", ip);
-        //    }
-        //}
     }
-    
 }
